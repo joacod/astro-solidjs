d9ee4eb09cd69966d9c4234e5e7d215ff625dfdc
Joaquin Diaz <joacod@gmail.com>
2023-11-07 01:27:10 -0300

    button component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,9 +5,17 @@ export default {
     extend: {
       colors: {
         primary: "#FCFFE7",
+        "primary-focus": "#E2E5CF",
+        "primary-active": "#C9CCB8",
         secondary: "#BAD7E9",
+        "secondary-focus": "#A7C1D1",
+        "secondary-active": "#94ACBA",
         accent: "#EB455F",
+        "accent-focus": "#D33E55",
+        "accent-active": "#BC374C",
         dark: "#2B3467",
+        "dark-focus": "#262E5C",
+        "dark-active": "#222952",
       },
     },
   },

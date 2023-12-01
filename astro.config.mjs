2d94de79f8254a3258ea2189057ecfb2419139b2
Joaquin Diaz <joacod@gmail.com>
2023-12-01 12:45:27 +0100

    vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
+import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
   integrations: [tailwind(), solidJs()],
-  output: "hybrid",
+  output: "server",
+  adapter: vercel(),
 });
